1478206795b307dafc35dd62b5b853e09bde345f
Ahmed Manssur <AhmedManssur@outlook.com>
2022-05-22 23:15:49 +0200

    Added a connection to SQL local database.

--- Publisher.Data/PublisherContext.cs
+++ Publisher.Data/PublisherContext.cs
@@ -8,5 +8,10 @@ namespace Publisher.Data
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog = PublisherDatabase");
+        }
     }
 }

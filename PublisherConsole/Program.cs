a42342d22815b0bee5107c7a61312d814076b7cf
Ahmed Manssur <AhmedManssur@outlook.com>
2022-05-23 00:13:50 +0200

    Filter a query with a hard coded value directly.

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,74 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.EntityFrameworkCore;
 using Publisher.Data;
-using Publisher.Domain;
 
-using (PublisherContext context = new PublisherContext())
-{
-    context.Database.EnsureCreated();
-}
+GetFilteredData();
 
-//GetAuthors();
-//AddAuthor();
-//GetAuthors();
-
-AddAuthorWithBooks();
-GetAuthorWithBooks();
-
-void AddAuthor()
+void GetFilteredData()
 {
     using var context = new PublisherContext();
 
-    var author = new Author { FirstName = "Ahmed", LastName = "Mansour" };
+    var authors = context.Authors.Where(x => x.FirstName == "Ahmed").ToList();
 
-    context.Authors.Add(author);
-
-    context.SaveChanges();
-}
-
-void GetAuthors()
-{
-    using var context = new PublisherContext();
-
-    var authors = context.Authors.ToList();
     foreach (var author in authors)
     {
         Console.WriteLine(author.FirstName + " " + author.LastName);
-    }
-}
-
-void AddAuthorWithBooks()
-{
-    var author = new Author
-    {
-        FirstName = "Ahmed",
-        LastName = "Mansour"
-    };
-
-    author.Books = new List<Book>
-    {
-        new Book{ Title = "Programming with C#", PublishDate = new DateTime(2022, 1,1)},
-        new Book{ Title = "Programming with Java", PublishDate = new DateTime(2022, 2,1)},
-    };
-
-    using var context = new PublisherContext();
-
-    context.Add(author);
-    context.SaveChanges();
-}
-
-void GetAuthorWithBooks()
-{
-    using var context = new PublisherContext();
-
-    var authors = context.Authors.Include(a => a.Books).ToList();
-
-    foreach (var author in authors)
-    {
-        Console.WriteLine(author.FirstName + "" + author.LastName + " Owning these books: ");
-        foreach (var book in author.Books)
-        {
-            Console.WriteLine(book.Title);
-        }
     }
 }

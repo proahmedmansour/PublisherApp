56019ddd1a1c1aca4e309b75825c1ca19d688acf
Ahmed Manssur <AhmedManssur@outlook.com>
2022-05-22 23:29:17 +0200

    Added some easy examples in reading and writing from database.

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Publisher.Data;
+using Publisher.Domain;
+
+using (PublisherContext context = new PublisherContext())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+AddAuthor();
+GetAuthors();
+
+void AddAuthor()
+{
+    using var context = new PublisherContext();
+
+    var author = new Author { FirstName = "Ahmed", LastName = "Mansour" };
+
+    context.Authors.Add(author);
+
+    context.SaveChanges();
+}
+
+void GetAuthors()
+{
+    using var context = new PublisherContext();
+
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}

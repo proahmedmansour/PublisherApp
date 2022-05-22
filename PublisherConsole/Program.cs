3845535decf004dc25006a9adb46d470f02189a9
Ahmed Manssur <AhmedManssur@outlook.com>
2022-05-22 23:50:19 +0200

    Added simple example to add author with books and also getting author with books.

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
 using Publisher.Data;
 using Publisher.Domain;
 
@@ -7,9 +8,12 @@ using (PublisherContext context = new PublisherContext())
     context.Database.EnsureCreated();
 }
 
-GetAuthors();
-AddAuthor();
-GetAuthors();
+//GetAuthors();
+//AddAuthor();
+//GetAuthors();
+
+AddAuthorWithBooks();
+GetAuthorWithBooks();
 
 void AddAuthor()
 {
@@ -30,5 +34,41 @@ void GetAuthors()
     foreach (var author in authors)
     {
         Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
+
+void AddAuthorWithBooks()
+{
+    var author = new Author
+    {
+        FirstName = "Ahmed",
+        LastName = "Mansour"
+    };
+
+    author.Books = new List<Book>
+    {
+        new Book{ Title = "Programming with C#", PublishDate = new DateTime(2022, 1,1)},
+        new Book{ Title = "Programming with Java", PublishDate = new DateTime(2022, 2,1)},
+    };
+
+    using var context = new PublisherContext();
+
+    context.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthorWithBooks()
+{
+    using var context = new PublisherContext();
+
+    var authors = context.Authors.Include(a => a.Books).ToList();
+
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + "" + author.LastName + " Owning these books: ");
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine(book.Title);
+        }
     }
 }

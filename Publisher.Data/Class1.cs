917f386a73176e87b4ee40913600ec22e1f9df7b
Ahmed Manssur <AhmedManssur@outlook.com>
2022-05-22 23:09:41 +0200

    Added data model using EF Core.

--- Publisher.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Publisher.Data
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Publisher.Data/PublisherContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Publisher.Domain;
+
+namespace Publisher.Data
+{
+    // Provides EF Core's database connectivity and tracks changes to objects
+    public class PublisherContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
